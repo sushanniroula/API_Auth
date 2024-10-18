9545f52e3a5cfad359f39fb4af3a6910bfe8c631
sushan <sushanniroula@gmail.com>
2024-10-18 19:42:14 +0545

    access token and refresh token added to access protected route

--- helpers/jwt_helper.js
+++ helpers/jwt_helper.js
@@ -1,16 +1,13 @@
 const JWT = require('jsonwebtoken')
 const createError = require('http-errors')
 
-
-
-
 module.exports = {
     signAccessToken: (userId) => {
         return new Promise((resolve, reject) => {
             const payload = {}
             const secret = process.env.ACCESS_TOKEN_SECRET
             const options ={
-                expiresIn: "15s",
+                expiresIn: "30s",
                 issuer: "domainurl.com",
                 audience: userId
             }
@@ -43,4 +40,31 @@ module.exports = {
             next()
         })
     },
+    signRefreshToken: (userId) => {
+        return new Promise((resolve, reject) => {
+            const payload = {}
+            const secret = process.env.REFRESH_TOKEN_SECRET
+            const options ={
+                expiresIn: "1y",
+                issuer: "domainurl.com",
+                audience: userId
+            }
+
+            JWT.sign(payload, secret, options, (err, token) => {
+                if(err){
+                    reject(createError.InternalServerError())
+                }
+                resolve(token)
+            })
+        })
+    },
+    verifyRefreshToken: (refreshToken) => {
+        return new Promise((resolve, reject) => {
+            JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET,(err, payload) =>{
+                if(err) return reject(createError.Unauthorized())
+                const userId = payload.aud
+                resolve(userId)
+            })
+        })
+    }
 }
