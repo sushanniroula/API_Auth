1b4102878d8e5aaea0e3a66e9d2db41c4e355aeb
sushan <sushanniroula@gmail.com>
2025-03-14 00:49:16 +0545

    product view, add, delete, update api created

--- backend/Routes/product.route.js
+++ backend/Routes/product.route.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { createProduct, getAllProducts, viewProduct } = require('../controllers/Product.controller')
+const { createProduct, getAllProducts, viewProduct, deleteProduct,  editProduct } = require('../controllers/Product.controller')
 
 router.post('/', createProduct)
 router.get('/all/:proId', getAllProducts)
 router.get("/:proId/:productId", viewProduct)
+router.delete("/:proId/:productId", deleteProduct)
+router.put('/', editProduct)
 
 module.exports = router
--- backend/controllers/Product.controller.js
+++ backend/controllers/Product.controller.js
@@ -1,53 +1,54 @@
 const express = require("express");
 const Product = require("../Models/Product.model");
-const User = require('../Models/User.model')
+const User = require("../Models/User.model");
 const userExists = async (proId) => {
   try {
-    return await User.exists({_id: proId})
+    return await User.exists({ _id: proId });
   } catch (error) {
-    return 
-    
+    return;
   }
-}
+};
 
-exports.createProduct = async(req, res)=>{
-    const {proId, name, category, description, price } = req.body
-    try {
-        const createProducts = await Product.create({
-            customerOf: proId,
-            name,
-            category,
-            price,
-            description
-            
-        })
-        if(!createProducts) return res.status(500).json({message: "Couldnot create product"})
-        
-        res.status(201).json({message: "Product Created", product: createProducts})
-    } catch (error) {
-        res.status(500).json({message: "Couldnot create product", error: error.message})
-    }
-}
+exports.createProduct = async (req, res) => {
+  const { proId, name, category, description, price } = req.body;
+  try {
+    const createProducts = await Product.create({
+      customerOf: proId,
+      name,
+      category,
+      price,
+      description,
+    });
+    if (!createProducts)
+      return res.status(500).json({ message: "Couldnot create product" });
 
-exports.getAllProducts = async(req, res) => {
-    const { proId }  = req.params
-    
-    // const exists = await userExists(proId)
-    // if(!exists) return res.status(400).json({message: "User not exist"})
-    try {
-        const products = await Product.find({customerOf: proId})
-        res.status(200).json(products)
-    }
-    
-    catch (error) {
-        res.status(500).json({message: "Failed to load"})
-    }
-}
+    res
+      .status(201)
+      .json({ message: "Product Created", product: createProducts });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Couldnot create product", error: error.message });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  const { proId } = req.params;
+
+  // const exists = await userExists(proId)
+  // if(!exists) return res.status(400).json({message: "User not exist"})
+  try {
+    const products = await Product.find({ customerOf: proId });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to load" });
+  }
+};
 
 exports.viewProduct = async (req, res) => {
   const { proId, productId } = req.params;
-  const exists = await userExists(proId)
-  if(!exists) return res.status(400).json({message: "User not exist"})
+  const exists = await userExists(proId);
+  if (!exists) return res.status(400).json({ message: "User not exist" });
   try {
     const productDetails = await Product.findOne({
       _id: productId,
@@ -61,5 +62,46 @@ exports.viewProduct = async (req, res) => {
     res
       .status(500)
       .json({ message: "Failed to retrive customer", error: error.message });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  const { proId, productId } = req.params;
+  const exists = await userExists(proId);
+  if (!exists) return res.status(400).json({ message: "User not exist" });
+
+  try {
+    const deleteProduct = await Product.findOneAndDelete({
+      customerOf: proId,
+      _id: productId,
+    });
+    if (!deleteProduct)
+      return res.status(404).json({ message: "Product not exist" });
+
+    res.status(200).json({ message: "Product deleted" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to delete product", error: error.message });
+  }
+};
+
+exports.editProduct = async (req, res) => {
+  try {
+    const { proId, productId, name, category, price, description, addedAt } = req.body;
+    console.log(req.body)
+    const exists = await userExists(proId);
+    if (!exists) return res.status(400).json({ message: "User not exist" });
+
+    const editProduct = await Product.findOneAndUpdate(
+      { customerOf: proId, _id: productId },
+      {name, category, price, description, addedAt},
+      {new: true, runValidators: true}
+    )
+    if(!editProduct) return res.status(400).json({message: "Product could not updated"})
+    
+    res.status(200).json({message: "Product updated"})
+  } catch (error) {
+    res.status(500).json({message: "Failed to update", error: error.message})
   }
 };
