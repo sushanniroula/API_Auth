46acba7743016a9659bb543ae9a11d9a60cc1aae
sushan <sushanniroula@gmail.com>
2025-03-16 22:43:34 +0545

    customer routes modified

--- backend/Routes/customer.route.js
+++ backend/Routes/customer.route.js
@@ -1,19 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const { verifyAccessToken } = require('../helpers/jwt_helper')
-const{ 
-    addCustomer,
-    viewCustomer,
-    editCustomer,
-    deleteCustomer,
-    getAllCustomer
-} = require('../controllers/Customer.controller')
+const express = require("express");
+const router = express.Router();
+const { verifyAccessToken } = require("../helpers/jwt_helper");
+const {
+  addCustomer,
+  viewCustomer,
+  editCustomer,
+  deleteCustomer,
+  getAllCustomer,
+} = require("../controllers/Customer.controller");
 
-router.post("/", verifyAccessToken, addCustomer)
-router.get("/:proId/:customerId", verifyAccessToken, viewCustomer)
-router.get("/:proId", verifyAccessToken, getAllCustomer) //completed
-router.put("/", verifyAccessToken, editCustomer)
-router.delete("/:id", verifyAccessToken, deleteCustomer
-)
+router.post("/", verifyAccessToken, addCustomer);
+router.get("/:proId/:customerId", verifyAccessToken, viewCustomer);
+router.get("/:proId", verifyAccessToken, getAllCustomer); //completed
+router.put("/", verifyAccessToken, editCustomer);
+router.delete("/:id", verifyAccessToken, deleteCustomer);
 
-module.exports = router
+module.exports = router;
